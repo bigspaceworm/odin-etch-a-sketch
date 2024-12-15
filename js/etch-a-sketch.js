3490207db7ac57d633596c7611b0ba1420adf947
Alex <114047696+bigspaceworm@users.noreply.github.com>
2024-12-14 22:03:58 -0600

    Add framework for selectable themes
    The colors of the UI had been compartmentalized in the CSS to prepare
    for the addition of themes and the js can support it.

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -2,18 +2,22 @@ const BLOCK_SIZE = 15;
 const GRID_SIDE = 550;
 const BLOCK_INIT_OPACITY = 0.1;
 let colorMode = "solid";
+// Themes = GreenLCD, BlueLCD, ...
+let themeColor = "GreenLCD";
 const sketchContainer = document.querySelector("#sketchContainer");
 sketchContainer.style.height = GRID_SIDE + "px";
 sketchContainer.style.width = GRID_SIDE + "px";
+
+// const gridBlocks = sketchContainer.querySelectorAll("div");
 
 const btnOpacity = document.querySelector("#additiveOpacity").querySelector("button");
 const btnSolid = document.querySelector("#solidColor").querySelector("button");
 
 // Initial grid
+setUITheme();
 createGrid(16);
 
 function gridColorMode() {
-
 	const gridBlocks = sketchContainer.querySelectorAll("div");
 
 	gridBlocks.forEach((div) => {
@@ -30,14 +34,39 @@ function gridColorMode() {
 	});
 }
 
+function setBlockTheme() {
+	const gridBlocks = sketchContainer.querySelectorAll("div");
+
+	gridBlocks.forEach((div) => {
+		div.className = "block" + themeColor;
+	});
+}
+
+function setUITheme() {
+	const buttons = document.querySelectorAll("button");
+	const uiContainer = document.querySelector("#uiContainer");
+	const inputSizeField = uiContainer.querySelector("input");
+
+	buttons.forEach((button) => {
+		button.className = "button" + themeColor;
+	});
+	uiContainer.className = "uiContainer" + themeColor;
+	inputSizeField.className = "input" + themeColor;
+	sketchContainer.className = "sketchContainer" + themeColor;
+}
+
+function changeTheme() {
+	setUITheme();
+	setBlockTheme();
+}
+
 function createGrid(gridSize) {
 	let blockSide = GRID_SIDE / gridSize;
 	changeGridInfo(true, gridSize);
 
 	for(let i = 0; i < gridSize * gridSize; i++){
 		const block = document.createElement("div");
-		block.classList.add("block");
-		block.classList.add("blockSolidColor");
+		block.classList.add("block" + themeColor);
 		block.style.opacity = BLOCK_INIT_OPACITY;
 		block.style.height = blockSide + "px";
 		block.style.width = blockSide + "px";
